235f4d6877ccf74a89eca1fb50bad94b5fd2c8c5
Venkat-Sai-Neeldha <144706211+Venkat-Sai-Neeldha@users.noreply.github.com>
2025-04-11 11:51:53 +0530

    Update welcome.js

--- backend/routes/welcome.js
+++ backend/routes/welcome.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { islogin } = require('../controllers/logincontroller.js');
 
 router.post('/', islogin, (req, res) => {
-  // req.user already contains email from the JWT token
+ 
   res.json({ 
     valid: true, 
     user: {
